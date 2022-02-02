6da1e788adee5040ffd88cd9e928e5916da0bbed
AdrianaSantorio <adriana.santorio@gmail.com>
2022-02-02 16:30:58 +0100

    filterContacts first draft

--- js/script.js
+++ js/script.js
@@ -119,8 +119,21 @@ const root = new Vue ({
           }
         },
         filterContacts() {
-          console.log(this.searchString);
-        }
+          const searchString = this.searchString.split('');
+          console.log(searchString);
+
+          for (let z=0; z < this.contacts.length; z++) {
+            const nameArray = this.contacts[z].name.toLowerCase().split('');
+            console.log(nameArray);
+            this.contacts[z].visible = false;
+            for (let i=0; i < searchString.length; i++) {
+              if (nameArray.includes(searchString[i])) {
+                this.contacts[z].visible = true;
+              }
+            }
+         }
+        },
+        
     },
 
 });
