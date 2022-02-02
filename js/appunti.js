b844b198621d08f04b7e5fce81b6cd24f952acf4
AdrianaSantorio <adriana.santorio@gmail.com>
2022-02-02 17:19:55 +0100

    added refactored filtercontacts method

--- js/appunti.js
+++ js/appunti.js
@@ -1,21 +1,25 @@
+// filterContacts() {
+        //   const searchString = this.searchString.split('');
+        //   console.log(searchString);
+
+        //   for (let z=0; z < this.contacts.length; z++) {
+        //     const nameArray = this.contacts[z].name.toLowerCase().split('');
+        //     console.log(nameArray);
+        //     this.contacts[z].visible = false;
+        //     for (let i=0; i < searchString.length; i++) {
+        //       if (nameArray.includes(searchString[i])) {
+        //         this.contacts[z].visible = true;
+        //       }
+        //     }
+        //  }
+        // },
 
 
-filterContacts() {
-    const searchString = this.searchString.split('');
-    console.log(searchString);
 
-    for (let z=0; z < this.contacts.length; z++) {
-       
-       const campione = this.contacts[z].name
-       for (let i=0; i < searchString.length; i++) {
-       
-       
-       
-        includes(searchString[i]);
-       
-       }
-    }
-     
-     includes(searchString[i]);
-    }
-}
+        toShow(contact) {
+            if (!this.searchString) {
+              return true;
+            } else {
+              return contact.name.toLowerCase().includes(this.searchString.toLowerCase());
+            }
+        }
--- js/script.js
+++ js/script.js
@@ -118,29 +118,17 @@ const root = new Vue ({
             this.contacts[this.currentIndex].messages.push(newMessage);
           }
         },
-        // filterContacts() {
-        //   const searchString = this.searchString.split('');
-        //   console.log(searchString);
+        filterContacts() {
+          this.contacts.map((contact) => {
+            contact.visible = false;
 
-        //   for (let z=0; z < this.contacts.length; z++) {
-        //     const nameArray = this.contacts[z].name.toLowerCase().split('');
-        //     console.log(nameArray);
-        //     this.contacts[z].visible = false;
-        //     for (let i=0; i < searchString.length; i++) {
-        //       if (nameArray.includes(searchString[i])) {
-        //         this.contacts[z].visible = true;
-        //       }
-        //     }
-        //  }
-        // },
-        toShow(contact) {
-          if (!this.searchString) {
-            return true;
-          } else {
-            return contact.name.toLowerCase().includes(this.searchString.toLowerCase());
-          }
-        }
-        
+            if (!this.searchString) {
+              contact.visible = true;
+            } else if (contact.name.toLowerCase().includes(this.searchString.toLowerCase())) {
+              contact.visible = true;
+            }
+          })
+        }, 
     },
 
 });
