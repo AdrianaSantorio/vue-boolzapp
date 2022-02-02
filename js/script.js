7d20bb30a79f520713bcb31a171726427cff0aa6
AdrianaSantorio <adriana.santorio@gmail.com>
2022-02-02 16:47:08 +0100

    ticket

--- js/script.js
+++ js/script.js
@@ -118,21 +118,28 @@ const root = new Vue ({
             this.contacts[this.currentIndex].messages.push(newMessage);
           }
         },
-        filterContacts() {
-          const searchString = this.searchString.split('');
-          console.log(searchString);
+        // filterContacts() {
+        //   const searchString = this.searchString.split('');
+        //   console.log(searchString);
 
-          for (let z=0; z < this.contacts.length; z++) {
-            const nameArray = this.contacts[z].name.toLowerCase().split('');
-            console.log(nameArray);
-            this.contacts[z].visible = false;
-            for (let i=0; i < searchString.length; i++) {
-              if (nameArray.includes(searchString[i])) {
-                this.contacts[z].visible = true;
-              }
-            }
-         }
-        },
+        //   for (let z=0; z < this.contacts.length; z++) {
+        //     const nameArray = this.contacts[z].name.toLowerCase().split('');
+        //     console.log(nameArray);
+        //     this.contacts[z].visible = false;
+        //     for (let i=0; i < searchString.length; i++) {
+        //       if (nameArray.includes(searchString[i])) {
+        //         this.contacts[z].visible = true;
+        //       }
+        //     }
+        //  }
+        // },
+        toShow(contact) {
+          if (!this.searchString) {
+            return true;
+          } else {
+            return contact.name.toLowerCase().includes(this.searchString.toLowerCase());
+          }
+        }
         
     },
 
